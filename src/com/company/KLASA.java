0cc63f52ff2cd49a1aa35942806edb9e042a8814
butterfly983 <majamihailova@gmail.com>
2023-05-24 20:22:13 +0200

    Revert "test revert"
    This reverts commit c0381ceae3b9a09ea760e578717812c5abcc72fe.

--- src/com/company/KLASA.java
+++ src/com/company/KLASA.java
@@ -9,10 +9,8 @@ public class KLASA {
         sayGoodBye("Maja");
         System.out.println("Learning Git");
         System.out.println("adding commits using Intellj");
-        System.out.println("Hello Sanja");
-        System.out.println("test");
+        System.out.println("Sanja");
         
-
 
 
 
