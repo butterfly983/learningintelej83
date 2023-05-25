1fa210ac3ba0ef3544a8edf573c471410bce086f
butterfly983 <majamihailova@gmail.com>
2023-05-25 13:52:58 +0200

    added one registration

---
+++ src/com/company/Regstrationtest.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Regstrationtest {
+
+    public static void main(String [] args) {
+        //adding automation test
+
+        System.out.println("added first positive test");
+    }
+
+
+
+
+}
